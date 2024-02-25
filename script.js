6788054459ceb6f0276707b8516aa77cfd7f92ce
derajsp <DeraJSP@gmail.com>
2024-02-25 22:50:19 +0100

    added date and time

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
 const uList = document.querySelector('ol');
-const inputField = document.querySelector('input');
+const inputField = document.querySelector('.task-desc');
+const timeField = document.querySelector('.date-time');
 const addTask = document.querySelector('button');
 const finished = document.querySelector('ul');
 const newLine = document.createElement('br');
 
-addTask.addEventListener('click', addToList);
+addTask.addEventListener('click', () => {
+
+  // checks to see if field is empty
+  
+  if (inputField.value !== '') {
+    addToList();
+  } else {
+    //do nothing
+ }
+});
+
+// adds task to the list
 
 function addToList() {
-  const newTask = inputField.value;
+  let newTask = inputField.value;
   inputField.value = '';
+  let time = timeField.value;
+  const newTime = time.replace('T', ' \ |  ');
   const task = document.createElement('li');
   const taskSpan = document.createElement('span');
+  const timeSpan = document.createElement('span');
+  taskSpan.style.setProperty('font-weight', '600')
   taskSpan.className = 'taskText';
   const deleteTask = document.createElement('button');
   const editTask = document.createElement('button');
@@ -19,17 +35,22 @@ function addToList() {
   editTask.className = 'editbtn'
   deleteTask.textContent = 'Delete';
   editTask.textContent = 'Edit';
-  taskCheck.setAttribute('type', "checkbox");
-  
+  taskCheck.setAttribute('type', "checkbox");  
   const divCheck = document.createElement('div');
   divCheck.className = 'mod-task';
-
+  const timeDiv = document.createElement('div');
+  timeDiv.className = 'timeWrap'
   task.appendChild(taskCheck)
   task.appendChild(taskSpan);
+  timeDiv.appendChild(timeSpan);
+  task.appendChild(timeDiv);
   divCheck.appendChild(editTask);
   divCheck.appendChild(deleteTask);
   task.appendChild(divCheck);
   taskSpan.textContent = `${newTask}`;
+  timeSpan.textContent = `${newTime}`;
+  timeSpan.style.setProperty('color', '#7393B3');
+  timeSpan.style.setProperty('font-weight', '500');
   uList.appendChild(task);
 
   deleteTask.addEventListener('click', function() {
@@ -56,11 +77,13 @@ taskCheck.addEventListener('click', function(){
     taskSpan.style.setProperty('text-decoration', 'line-through');
     taskSpan.style.setProperty('color', 'red');
     divCheck.remove()
+    timeDiv.remove()
     } else {
       task.remove()
       uList.appendChild(task);
       task.appendChild(taskCheck);
       task.appendChild(taskSpan);
+      task.appendChild(timeDiv);
       divCheck.appendChild(editTask);
       divCheck.appendChild(deleteTask);
       task.appendChild(divCheck);
